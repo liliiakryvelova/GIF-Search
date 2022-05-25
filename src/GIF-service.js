5da52178d72fa47802800b105cf2405c89c0afaf
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-05-24 19:23:11 -0700

    add functionality for trending content

--- src/GIF-service.js
+++ src/GIF-service.js
@@ -14,4 +14,28 @@ export default class GIFservice {
       request.send();
     });
   }
+
 }
+
+
+export class GIFservice2 {
+  static getTrending() {
+    return new Promise(function (resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=4&rating=g`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
+
+https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=&rating=g
+
+
--- src/index.js
+++ src/index.js
@@ -3,20 +3,34 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import GIFservice from './GIF-service.js';
+import { GIFservice2 } from './GIF-service.js';
 
 function clearFields() {
   $('#keyword').val("");
   $('.showErrors').text("");
-  $('.showResult').text("");
+  $('.showResult').append("");
+  $('.showTrendingResult').append("");
 }
 
 
 $(document).ready(function () {
+  let promise2 = GIFservice2.getTrending();
+  promise2.then(function (response) {
+    const body = JSON.parse(response);
+    body.data.forEach(gif => {
+      $(`.showTrendingResult`).append(`<div class="card" style="width: 25%;">
+      <img src="${gif.images.fixed_height.url}" class="card-img-top" alt="${gif.title}">
+      <div class="card-body">
+        <p class="card-text">${gif.title}</p>
+      </div>
+    </div>`);
+    });
+  });
+
   $('#searchResult').click(function () {
     let keyword = $(`#keyword`).val();
     clearFields();
     let promise = GIFservice.getPicture(keyword);
-
 
     promise.then(function (response) {
       const body = JSON.parse(response);
